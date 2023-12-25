456bac24b71c8539566da0f4ec68d6c943ca7bd1
s3ansh33p <newfolderlocation@gmail.com>
2023-12-25 14:56:17 +0000

    Cleanup

--- SkyNet/chatg.lua
+++
@@ -1,6 +0,0 @@
-local chatBox = peripheral.find("chatBox")
-
--- send message that "s3nsuous was shot by Skeleton"
-chatBox.sendMessage("s3nsuous was shot by Skeleton", "")
--- send to the same player
--- chatBox.sendMessageToPlayer("s3nsuous was shot by Skeleton", "s3nsuous")
